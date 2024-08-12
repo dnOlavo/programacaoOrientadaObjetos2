db648727fa09d388eabf2e3af87aa63d8875e3cc
Olavo Nunes <120687522+dnOlavo@users.noreply.github.com>
2024-08-12 16:25:46 -0300

    Cria as classes ClienteFisico e ClienteJuridico que implementam aplicarDesconto

---
+++ encapsulamento/src/main/java/iftm/edu/br/dnolavo/encapsulamento/ClienteFisico.java
@@ -0,0 +1,13 @@
+package iftm.edu.br.dnolavo.encapsulamento;
+
+public class ClienteFisico extends Cliente {
+
+    public ClienteFisico(String nome) {
+        super(nome);
+    }
+
+    @Override
+    public double aplicarDesconto(double precoBase) {
+        return precoBase * 0.9; // 10% de desconto
+    }
+}
---
+++ encapsulamento/src/main/java/iftm/edu/br/dnolavo/encapsulamento/ClienteJuridico.java
@@ -0,0 +1,13 @@
+package iftm.edu.br.dnolavo.encapsulamento;
+
+public class ClienteJuridico extends Cliente {
+
+    public ClienteJuridico(String nome) {
+        super(nome);
+    }
+
+    @Override
+    public double aplicarDesconto(double precoBase) {
+        return precoBase * 0.8; // 20% de desconto
+    }
+}
