fefc97fa089fc6b5551f7311b374fb26b35cfd1d
Olavo Nunes <120687522+dnOlavo@users.noreply.github.com>
2024-08-12 16:18:43 -0300

    Cria o diretório do Exercício de Encapsulamento para ser Refatorado

---
+++ encapsulamento/src/main/java/iftm/edu/br/dnolavo/encapsulamento/EncapsulamentoApplication.java
@@ -0,0 +1,23 @@
+package iftm.edu.br.dnolavo.encapsulamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncapsulamentoApplication {
+
+	public static void main(String[] args) {
+        Produto produto = new Produto("Notebook", 3000.00);
+
+        Cliente cliente1 = new Cliente("João", "PessoaFisica");
+        Cliente cliente2 = new Cliente("Empresa X", "PessoaJuridica");
+
+        Precificacao pricingEngine = new Precificacao();
+
+        double precoFinalPessoaFisica = pricingEngine.calcularPrecoFinal(produto, cliente1);
+        double precoFinalPessoaJuridica = pricingEngine.calcularPrecoFinal(produto, cliente2);
+
+        System.out.println("Preço final para " + cliente1.getNome() + ": " + precoFinalPessoaFisica);
+        System.out.println("Preço final para " + cliente2.getNome() + ": " + precoFinalPessoaJuridica);
+    }
+}
---
+++ encapsulamento/src/test/java/iftm/edu/br/dnolavo/encapsulamento/EncapsulamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package iftm.edu.br.dnolavo.encapsulamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncapsulamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
