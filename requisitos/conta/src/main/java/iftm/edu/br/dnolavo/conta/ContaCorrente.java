45209fbbf1a76c8f7af60bc3f435a395baed2ed7
Olavo Nunes <120687522+dnOlavo@users.noreply.github.com>
2024-08-19 20:47:48 -0300

    Implementa a interface Tarifavel na ContaCorrente e adiciona a lógica para a cobrança de tarifa.

--- requisitos/conta/src/main/java/iftm/edu/br/dnolavo/conta/ContaCorrente.java
+++ requisitos/conta/src/main/java/iftm/edu/br/dnolavo/conta/ContaCorrente.java
@@ -1,26 +1,34 @@
 package iftm.edu.br.dnolavo.conta;
 
-public class ContaCorrente extends Conta {
+public class ContaCorrente extends Conta implements Tarifavel {
 
-    private double limiteChequeEspecial;
+   private double limiteChequeEspecial;
+   private static final double TARIFA = 15.0; // Exemplo de valor de tarifa
 
-    public ContaCorrente(Agencia agencia, String numeroConta, double saldo,
-                         double limiteChequeEspecial) {
-        super(agencia,numeroConta,saldo);
-        this.limiteChequeEspecial = limiteChequeEspecial;
-    }
-    
-    @Override
-    public void saque(double valor) throws ContaException {
-        if (valor > 0) {
+   public ContaCorrente(Agencia agencia, String numeroConta, double saldo, double limiteChequeEspecial) {
+       super(agencia, numeroConta, saldo);
+       this.limiteChequeEspecial = limiteChequeEspecial;
+   }
+   
+   @Override
+   public void saque(double valor) throws ContaException {
+       if (valor > 0) {
            if (this.saldo + limiteChequeEspecial >= valor) {
-              this.saldo -= valor;
+               this.saldo -= valor;
            } else {
-              throw new ContaException("Conta sem saldo suficiente: "+this.saldo);
+               throw new ContaException("Conta sem saldo suficiente: " + this.saldo);
            }
-        } else {
-              throw new ContaException("Valor inválido para saque "+valor);
-        }
-     }
+       } else {
+           throw new ContaException("Valor inválido para saque " + valor);
+       }
+   }
 
+   @Override
+   public void cobrarTarifa() {
+       if (this.saldo >= TARIFA) {
+           this.saldo -= TARIFA;
+       } else {
+           System.out.println("Saldo insuficiente para cobrança da tarifa.");
+       }
+   }
 }
