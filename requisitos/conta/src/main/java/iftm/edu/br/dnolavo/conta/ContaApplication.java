24724b1ddced5fddb4995f9a522bad3071a1cd30
Olavo Nunes <120687522+dnOlavo@users.noreply.github.com>
2024-08-19 20:54:56 -0300

    Modifica o ContaApplication.java para incluir ContaSalario e aplicar cobrança de tarifa em ContaCorrente.

--- requisitos/conta/src/main/java/iftm/edu/br/dnolavo/conta/ContaApplication.java
+++ requisitos/conta/src/main/java/iftm/edu/br/dnolavo/conta/ContaApplication.java
@@ -6,13 +6,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ContaApplication {
 	public static void main(String[] args) throws Exception {
+
 		Agencia caixaLuizote = new Agencia("0205-08");
+
 		Conta contaCorrente = new ContaCorrente(caixaLuizote, "1164-9", 500d, 1000);
 		Conta contaPoupanca = new ContaPoupanca(caixaLuizote, "1324-9", 700d);
+		Conta contaSalario = new ContaSalario(caixaLuizote, "1424-5", 1200d);
+
 		caixaLuizote.addConta(contaPoupanca);
 		caixaLuizote.addConta(contaCorrente);
+		caixaLuizote.addConta(contaSalario);
+
 		caixaLuizote.saque("1164-9", 1200);
 		caixaLuizote.saque("1324-9", 1000);
+		caixaLuizote.saque("1424-5", 500);
+
+		// Cobrar tarifa das contas que são Tarifáveis
+		if (contaCorrente instanceof Tarifavel) {
+			((Tarifavel) contaCorrente).cobrarTarifa();
+		}
+
+		System.out.println("Saldo da Conta Corrente após tarifa: " + contaCorrente.getSaldo());
+		System.out.println("Saldo da Conta Poupança: " + contaPoupanca.getSaldo());
+		System.out.println("Saldo da Conta Salário: " + contaSalario.getSaldo());
 	}
 
 }
